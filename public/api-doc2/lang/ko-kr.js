74c282dd06d809a3dbcdaa4fbfd087a5132c387f
KhushagraSikka <khushagrasikka@gmail.com>
2025-05-17 14:34:00 +0000

    Update project documentation

---
+++ public/api-doc2/lang/ko-kr.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/* jshint quotmark: double */
+window.SwaggerTranslator.learn({
+    "Warning: Deprecated":"ê²½ê³ ï¼íê¸°ìì ë¨",
+    "Implementation Notes":"êµ¬í ë¸í¸",
+    "Response Class":"ìëµ í´ëì¤",
+    "Status":"ìí",
+    "Parameters":"ë§¤ê°ë³ìë¤",
+    "Parameter":"ë§¤ê°ë³ì",
+    "Value":"ê°",
+    "Description":"ì¤ëª",
+    "Parameter Type":"ë§¤ê°ë³ì íì",
+    "Data Type":"ë°ì´í° íì",
+    "Response Messages":"ìëµ ë©ì¸ì§",
+    "HTTP Status Code":"HTTP ìí ì½ë",
+    "Reason":"ìì¸",
+    "Response Model":"ìëµ ëª¨ë¸",
+    "Request URL":"ìì²­ URL",
+    "Response Body":"ìëµ ë³¸ë¬¸",
+    "Response Code":"ìëµ ì½ë",
+    "Response Headers":"ìëµ í¤ë",
+    "Hide Response":"ìëµ ì¨ê¸°ê¸°",
+    "Headers":"í¤ë",
+    "Try it out!":"ì¨ë³´ê¸°ï¼",
+    "Show/Hide":"ë³´ì´ê¸°/ì¨ê¸°ê¸°",
+    "List Operations":"ëª©ë¡ ìì",
+    "Expand Operations":"ì ê° ìì",
+    "Raw":"ìë³¸",
+    "can't parse JSON.  Raw result":"JSONì íì±í ì ìì. ìë³¸ê²°ê³¼:",
+    "Model Schema":"ëª¨ë¸ ì¤í¤ë§",
+    "Model":"ëª¨ë¸",
+    "apply":"ì ì©",
+    "Username":"ì¬ì©ì ì´ë¦",
+    "Password":"ìí¸",
+    "Terms of service":"ì´ì©ì½ê´",
+    "Created by":"ìì±ì",
+    "See more at":"ì¶ê°ì ë³´ï¼",
+    "Contact the developer":"ê°ë°ììê² ë¬¸ì",
+    "api version":"apië²ì ",
+    "Response Content Type":"ìëµContent Type",
+    "fetching resource":"ë¦¬ìì¤ ê°ì ¸ì¤ê¸°",
+    "fetching resource list":"ë¦¬ìì¤ ëª©ë¡ ê°ì ¸ì¤ê¸°",
+    "Explore":"íì",
+    "Show Swagger Petstore Example Apis":"Swagger Petstore ìì  ë³´ê¸°",
+    "Can't read from server.  It may not have the appropriate access-control-origin settings.":"ìë²ë¡ë¶í° ì½ì´ë¤ì¼ì ììµëë¤. access-control-origin ì¤ì ì´ ì¬ë°ë¥´ì§ ììì ììµëë¤.",
+    "Please specify the protocol for":"ë¤ìì ìí íë¡í ì½ì ì íì¸ì",
+    "Can't read swagger JSON from":"swagger JSON ì ë¤ìì¼ë¡ ë¶í° ì½ìì ììµëë¤",
+    "Finished Loading Resource Information. Rendering Swagger UI":"ë¦¬ìì¤ ì ë³´ ë¶ë¬ì¤ê¸° ìë£. Swagger UI ëëë§",
+    "Unable to read api":"apië¥¼ ì½ì ì ììµëë¤.",
+    "from path":"ë¤ì ê²½ë¡ë¡ ë¶í°",
+    "server returned":"ìë² ìëµí¨."
+});
+
+
+// Potential improvement: Add error handling
