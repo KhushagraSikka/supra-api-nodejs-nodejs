a21dbd1e87ef539831c3ff53e2711a68a6cb8e66
KhushagraSikka <khushagrasikka@gmail.com>
2025-06-05 16:33:43 +0000

    Polish code and formatting

---
+++ modules/root/RootController.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+const { BaseController } = require('../../rootcommmon/BaseController')
+const RootProvider = require('../RootProvider')
+
+class RootController extends BaseController {
+  get router () {
+    router.get('/', (req, res) => {
+      res.json({ success: true, message: '(>___<)' })
+    })
+
+    return router
+  }
+
+  async init () {
+    this.logger.debug(`${this.constructor.name} initialized...`)
+    await RootProvider.init()
+  }
+}
+
+module.exports = { RootController }
+
+
+// TODO: Consider refactoring for better performance
